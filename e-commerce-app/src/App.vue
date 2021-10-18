6d6d6a9921e874300405c7a438718d080f8228cc
Yunus Erol <erol_yunus@hotmail.com>
2021-10-19 00:52:40 +0300

    e-commerce-app files uploaded

---
+++ e-commerce-app/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="container-fluid d-flex justify-content-around flex-wrap align-items-center" >
+      <div class="card_main" v-for="(item,i)  in logitems" 
+      :key="i" >
+
+        <Product class="card__item" 
+        :name="item.name" 
+        :category="item.category" 
+        :price="item.price" 
+        :summary="item.summary" 
+        :color="item.color" 
+        :img="item.img"
+        />
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Product from './components/Product.vue'
+import items from './components/items'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Product,
+  },
+  data () {
+    return {
+        logitems: items
+      }
+  }
+}
+</script>
+
+<style >
+.container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+}
+.card_main{
+  margin-top: 50px;
+  box-sizing: border-box;
+  box-shadow: 1px 1px 5px slategray;
+  border-radius: 10px;
+ }
+
+</style>>
+
+
---
+++ e-commerce-app/src/components/Product.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="main-card">
+    <div class="product">
+        <div class="images">
+            <img v-bind:src="require('../assets/' + img)" />
+        </div>
+        <div class="choose-size"> 
+
+            <p :class="this.color">CHOOSE SIZE</p>
+
+            <div class="sizes" >
+
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">38</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">39</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">40</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">41</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">42</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">43</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">44</div>
+                <div class="size" :class="`border-color-${this.color} ${this.color}`">45</div>
+            </div>
+            </div>
+    </div>
+    <div class="summary">
+
+        <p class="head" 
+        :class="this.color">{{ category }}
+        </p>
+
+        <h1 class="main">{{ name }}</h1>
+
+        <h2 
+        :class="this.color">${{ price }}
+        </h2>
+
+        <Rating :color="color" :star="star"/>
+
+        <p class="desc">{{ summary }} </p>
+
+        <div class="buttons" 
+        :class="`bg-${this.color}`">
+
+            <div class="select-value">
+                <select  
+                :class="this.color" name="value" id="value">
+
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <button class="add white" 
+            :class="`bg-${this.color}`">Add to Cart</button>
+
+        </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Rating from './Rating.vue'
+
+
+export default {
+  name: 'Product',
+  props: {
+  name: String,
+  category: String,
+  summary:String,
+  color:String,
+  price:Number,
+  star:Number,
+  img:String
+},
+  components: {
+    Rating,
+  },
+       
+    
+}
+
+</script>
+<style>
+.pink{
+    color:#C3A1A0;
+}
+.blue{
+    color:#7ccde6;
+}
+.bg-pink{
+    background-color:#C3A1A0;
+}
+.bg-blue{
+    background-color:#7ccde6;
+}
+.white{
+    color: #F5F5F5;
+}
+
+.buttons{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.select-value{
+    border: hidden;
+    font-size: 14px;
+
+
+}
+select:focus{
+outline: none;
+}
+select{
+    border: hidden;
+    font-size: 14px;
+    width: 50px;
+    padding: 7px;
+    margin-right: 5px;
+}
+select-items{
+    border: hidden;
+    font-size: 14px;
+}
+button{
+    border: none;
+    padding: 5px;
+    display: inline-block;
+    outline: 0;
+    margin: -1px;
+    border-radius: 2px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+}
+.main-card{
+    box-sizing: border-box;
+    width: 520px;
+    height: 350px;
+    padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    background-color: #f6f4f5;
+    padding: 20px;
+}
+.product{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+
+    
+}
+.summary{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 10px;
+}
+.choose-size{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 11px;
+    margin-bottom:0;
+    margin-left: 20px;
+}
+.choose-size p{
+    margin-bottom: 1px;
+    font-size: .7rem;
+
+}
+.sizes{
+    display: flex;
+
+}
+.border-color-pink{
+    border: 1px solid #E0C9CB;
+}
+.border-color-blue{
+    border: 1px solid #7ccde6;
+}
+.size{
+    text-align: center;
+    padding: 9px;
+
+    font-size: 0.7em;
+    margin-right: 3px;
+}
+.size:hover{
+    background-color:#C3A1A0;
+}
+img {
+  width: 300px;
+}
+.head{
+    font-size: 14px;
+    margin-top:-5px ;
+}
+.main {
+  font-size: 1.2em;
+  margin-top: -15px;
+ 
+}
+.desc{
+    font-size: .7em;
+}
+.add{
+    width: 100%;
+}
+</style>
